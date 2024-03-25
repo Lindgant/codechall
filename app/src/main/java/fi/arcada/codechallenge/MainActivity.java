afeb86cc4a007eb6337e08225def3e60320ef72b
lindgant <lindgant@arcada.fi>
2024-03-25 15:05:33 +0200

    code chall 2

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,10 @@
 package fi.arcada.codechallenge;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +14,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView outputText;
+    EditText inputText;
+    double[] values = {1,2,4,5,6,7,8,9,0};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        outputText=findViewById(R.id.outText);
+        inputText=findViewById(R.id.inputText);
+
+        outputText.setText("Min app funkar!");
+
+
     }
+
+    public void buttonHandler(View view) {
+        String text =inputText.getText().toString();
+        outputText.setText(text);
+    }
+    public void calculate(View view){
+        String average = inputText.getText().toString();
+        outputText.setText(average);
+    }
+
+
 }
